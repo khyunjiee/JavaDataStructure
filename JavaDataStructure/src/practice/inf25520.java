62528e06d2375ada7dce8a13977031a6dc74572b
Kim HyunJi <khyunjiee@gmail.com>
2021-04-20 23:03:18 +0900

    update: 인프런 답변 코드

---
+++ JavaDataStructure/src/practice/inf25520.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class inf25520 {
+    static void myMethod(String fname) {
+        System.out.println(fname + " refsnes");
+    }
+
+    public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+    }
+}
